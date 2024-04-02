558017a3ccd6955d3ef7dedb74cd97fd2218ead0
Adar Azulay <adar@apiiro.com>
2024-04-02 12:59:39 +0300

    Restlet

---
+++ src/Restlet/Main.java
@@ -0,0 +1,18 @@
+import org.restlet.resource.Delete;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+public class RestletResource extends ServerResource {
+
+    @Get
+    public String hello() {
+        return "HelloWorld in Restlet, this is GET method response";
+    }
+
+    @Delete
+    public void bye() {
+        System.out.println("Bye Bye, DELETE method called");
+
+    }
+
+}
